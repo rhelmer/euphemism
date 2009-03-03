f1010cf607f673a4bc26f9ad2201d89f45408db1
Robert Helmer <robert@roberthelmer.com>
2009-03-02 20:52:56 -0800

    save working ,starting on history

--- main.py
+++ main.py
@@ -1,36 +1,81 @@
 import cgi
 import os
 
-from google.appengine.api import urlfetch
 from google.appengine.ext.webapp import template
-from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.api import memcache
+from google.appengine.ext import db
+
+class Container(db.Model):
+  dom_id = db.StringProperty()
+  date = db.DateTimeProperty(auto_now_add=True)
+  top = db.StringProperty()
+  left = db.StringProperty()
+  width = db.StringProperty()
+  height = db.StringProperty()
 
 class Save(webapp.RequestHandler):
-  def get(self):
-    self.response.headers['Content-Type'] = 'text/html'
-    self.response.out.write('hello get')
-
   def post(self):
-    data = self.request
+    container = Container()
+    container.dom_id = self.request.get('id')
+    container.top = self.request.get('top')
+    container.left = self.request.get('left')
+    container.width = self.request.get('width')
+    container.height = self.request.get('height')
+    container.put()
+
     self.response.headers['Content-Type'] = 'text/html'
-    self.response.out.write(data)
+    self.response.out.write(container.dom_id)
 
 class MainPage(webapp.RequestHandler):
   def get(self):
-    template_values = {}
+
+    post = 'This is the post'
+    comment1 = 'This is comment1'
+    comment2 = 'This is comment2'
+
+    p_top = '0px';
+    p_left = '0px';
+    p_width = '';
+    p_height = '';
+    c_top = '0px';
+    c_left = '0px';
+    c_width = '';
+    c_height = '';
+    p = Container.gql("WHERE dom_id='post' ORDER BY date DESC LIMIT 1").get()
+    c = Container.gql("WHERE dom_id='comments' ORDER BY date DESC LIMIT 1").get()
+    if  p != None:
+      p_top = p.top
+      p_left = p.left
+      p_width = p.width
+      p_height = p.height
+    if  c != None:
+      c_top = c.top
+      c_left = c.left
+      c_width = c.width
+      c_height = c.height
+    template_values = {
+      'post': post,
+      'comment1': comment1,
+      'comment2': comment2,
+      'c_top': c_top,
+      'c_left': c_left,
+      'c_width': c_width,
+      'c_height': c_height,
+      'p_top': p_top,
+      'p_left': p_left,
+      'p_width': p_width,
+      'p_height': p_height,
+      'history': Container.all().order('-date').fetch(10)
+    }
 
     path = os.path.join(os.path.dirname(__file__), 'index.html')
     self.response.out.write(template.render(path, template_values))
 
-application = webapp.WSGIApplication(
-                                     [('/', MainPage),
-                                      ('/save', Save)],
-                                     debug=True)
 
 def main():
+  application = webapp.WSGIApplication(
+    [('/', MainPage), ('/save', Save)], debug=True)
   run_wsgi_app(application)
 
 if __name__ == "__main__":
--- scripts/euphy-0.1.js
+++ scripts/euphy-0.1.js
@@ -1,12 +1,23 @@
-$(document).ready(function(){
-  $(".container").resizable();
-  $(".container").draggable({
-    stop: function(event, ui){
-      $.post("/save", {
-        id: ui.helper[0].id, 
-        left: ui.helper[0].style.left,
-        top: ui.helper[0].style.top,
-      });
-    }
+function save(event, ui){
+  $.post("/save", {
+    id: ui.helper[0].id, 
+    left: ui.helper[0].style.left,
+    top: ui.helper[0].style.top,
+    width: ui.helper[0].style.width,
+    height: ui.helper[0].style.height,
   });
+}
+
+$(document).ready(function(){
+  $(".container").resizable({
+    stop: save
+  });
+  $(".container").draggable({
+    stop: save
+  });
+/*
+  $(".container").keydown(function(event){
+    save('test');
+  });
+*/
 });
